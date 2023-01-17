33b28b3e92466804b926154a3a0b22428b2b6207
vbrakshe <122854077+vbrakshe@users.noreply.github.com>
2023-01-17 11:29:29 +0530

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')🥣 🥗 🐔 🥑🍞
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Bluerry Oatmeal')
 streamlit.text('🥗 kale, spanich & Ricket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
