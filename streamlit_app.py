f633331e5ad1934a9cda5d09c873e4a54816c29c
vbrakshe <122854077+vbrakshe@users.noreply.github.com>
2023-01-17 11:16:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Bluerry Oatmeal')
+streamlit.text('kale, spanich & Ricket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
+              
