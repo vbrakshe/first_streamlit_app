6fe629c77fb14cee698e3455bcee03993890ff38
vbrakshe <122854077+vbrakshe@users.noreply.github.com>
2023-01-17 11:33:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Bluerry Oatmeal')
 streamlit.text('🥗 kale, spanich & Ricket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')           
+streamlit.text('🥑🍞 Avocado Toast') 
+                
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
